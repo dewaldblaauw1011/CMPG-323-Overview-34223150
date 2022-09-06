e3e3d25b43548cb3db3047e2ba85dff47690b8eb
dewaldblaauw1011 <86536136+dewaldblaauw1011@users.noreply.github.com>
2022-09-06 16:29:41 +0200

    Scaffolding of Database done

---
+++ Project2/Project2/Models/ConnectedOfficeContext.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Project2.Models
+{
+    public partial class ConnectedOfficeContext : DbContext
+    {
+        public ConnectedOfficeContext()
+        {
+        }
+
+        public ConnectedOfficeContext(DbContextOptions<ConnectedOfficeContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Category> Category { get; set; }
+        public virtual DbSet<Device> Device { get; set; }
+        public virtual DbSet<Zone> Zone { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Integrated Security=SSPI;Persist Security Info=False;Initial Catalog=ConnectedOffice");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Category>(entity =>
+            {
+                entity.Property(e => e.CategoryId).ValueGeneratedNever();
+
+                entity.Property(e => e.DateCreated).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Device>(entity =>
+            {
+                entity.Property(e => e.DeviceId).ValueGeneratedNever();
+
+                entity.Property(e => e.DateCreated).HasDefaultValueSql("(getdate())");
+            });
+
+            modelBuilder.Entity<Zone>(entity =>
+            {
+                entity.Property(e => e.ZoneId).ValueGeneratedNever();
+
+                entity.Property(e => e.DateCreated).HasDefaultValueSql("(getdate())");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Project2/Project2/Models/Device.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace Project2.Models
+{
+    public partial class Device
+    {
+        [Key]
+        [Column("DeviceID")]
+        public Guid DeviceId { get; set; }
+        public string DeviceName { get; set; }
+        [Column("CategoryID")]
+        public Guid CategoryId { get; set; }
+        [Column("ZoneID")]
+        public Guid ZoneId { get; set; }
+        public string Status { get; set; }
+        public bool IsActvie { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime DateCreated { get; set; }
+    }
+}
