1f233423747d4431a5f8aa43ff1061f25e671446
dewaldblaauw1011 <86536136+dewaldblaauw1011@users.noreply.github.com>
2022-09-06 09:40:54 +0200

    Create UserRole.cs

---
+++ Project2API/Authentication/UserRole.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace JWTAuthentication.Authentication
+{
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
+
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
