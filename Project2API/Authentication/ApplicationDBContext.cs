626fabad1071d0076c34d6c356755883a114869c
dewaldblaauw1011 <86536136+dewaldblaauw1011@users.noreply.github.com>
2022-09-06 09:39:16 +0200

    Added code to ApplicatioDBContext so that sql can be done via SQLServer

---
+++ Project2API/Authentication/ApplicationDBContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWTAuthentication.Authentication
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
+
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
