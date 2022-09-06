578dafa795c78b65cf8913c4c53946ee767f90ba
dewaldblaauw1011 <86536136+dewaldblaauw1011@users.noreply.github.com>
2022-09-06 09:42:52 +0200

    Create LoginModel.cs

---
+++ Project2API/Authentication/LoginModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace JWTAuthentication.Authentication
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "User Name is required")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+    }
+}
+
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
