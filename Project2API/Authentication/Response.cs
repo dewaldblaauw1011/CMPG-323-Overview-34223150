8ddae2e7a1ad538b97449e13889931762ba2bc78
dewaldblaauw1011 <86536136+dewaldblaauw1011@users.noreply.github.com>
2022-09-06 09:43:40 +0200

    Create Response.cs

---
+++ Project2API/Authentication/Response.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace JWTAuthentication.Authentication
+{
+    public class Response
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+}
+
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
