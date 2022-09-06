beb28c719a0c121e980dbf6506acef04d000cd68
dewaldblaauw1011 <86536136+dewaldblaauw1011@users.noreply.github.com>
2022-09-06 09:31:53 +0200

    Added ApplicationUser.cs for username, password, email etc

---
+++ Project2API/ApplicationUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace JWTAuthentication.Authentication
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
+
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+}
